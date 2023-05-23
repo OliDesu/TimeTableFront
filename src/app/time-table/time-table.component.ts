eb4150630a3a46f3f4a4010e325cb0f20eae9f1b
aelmufti <ali.elmufti@hardis-group.com>
2023-05-23 13:59:43 +0200

    pre update

--- src/app/time-table/time-table.component.ts
+++ src/app/time-table/time-table.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-time-table',
@@ -7,7 +8,25 @@ import {Component} from '@angular/core';
 })
 
 
-export class TimeTableComponent {
+export class TimeTableComponent { times: string[] = [];
+  inputs: { [key: string]: string } = {};
 
+  constructor() {
+    this.generateTimeTable();
+  }
+
+  generateTimeTable() {
+    const startHour = 9;
+    const endHour = 18;
+    const interval = 30;
+
+    for (let hour = startHour; hour <= endHour; hour++) {
+      for (let minute = 0; minute < 60; minute += interval) {
+        const time = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+        this.times.push(time);
+        this.inputs[time] = '';
+      }
+    }
+  }
 
 }
